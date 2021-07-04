d4b039957c13ba5b14cac178a01d757a53c34ef1
guozhenwei <413880854@qq.com>
2021-07-04 17:34:14 +0800

    项目基本环境搭建

---
+++ src/store/modules/user.js
@@ -0,0 +1,12 @@
+export const state = {}
+export const mutations = {}
+export const actions = {}
+export const getter = {}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getter
+}
